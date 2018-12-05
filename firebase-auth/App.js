80a366eb30f1b4e29b041a18d5bdf86b73554efb
KevinAsuncion <kevinramosasuncion@gmail.com>
2018-12-05 12:34:29 -0800

    add firebase

--- firebase-auth/App.js
+++ firebase-auth/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import SignUpForm from './components/SignUpForm';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import firebase from "firebase";
+import SignUpForm from "./components/SignUpForm";
+import SignInForm from "./components/SignInForm";
 
 export default class App extends React.Component {
+  componentDidMount() {
+    const config = {
+      apiKey: "AIzaSyD-25cWr3VMewAMR25uLDFG25mABm94FoE",
+      authDomain: "one-time-password-db445.firebaseapp.com",
+      databaseURL: "https://one-time-password-db445.firebaseio.com",
+      projectId: "one-time-password-db445",
+      storageBucket: "one-time-password-db445.appspot.com",
+      messagingSenderId: "626002185324"
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <SignUpForm />
+        <SignInForm />
       </View>
     );
   }
@@ -15,8 +30,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "space-around"
+  }
 });
