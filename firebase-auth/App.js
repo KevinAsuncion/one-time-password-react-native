6a0234ac9e8b66ff679b8b8dd347ab450d3e9e4d
KevinAsuncion <kevinramosasuncion@gmail.com>
2018-12-05 12:54:18 -0800

    config update

--- firebase-auth/App.js
+++ firebase-auth/App.js
@@ -6,15 +6,7 @@ import SignInForm from "./components/SignInForm";
 
 export default class App extends React.Component {
   componentDidMount() {
-    const config = {
-      apiKey: "AIzaSyD-25cWr3VMewAMR25uLDFG25mABm94FoE",
-      authDomain: "one-time-password-db445.firebaseapp.com",
-      databaseURL: "https://one-time-password-db445.firebaseio.com",
-      projectId: "one-time-password-db445",
-      storageBucket: "one-time-password-db445.appspot.com",
-      messagingSenderId: "626002185324"
-    };
-    firebase.initializeApp(config);
+    //add config
   }
 
   render() {
